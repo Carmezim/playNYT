5dc2a9b7483f54272b68375b3d54bdfa80c38755
Carmezim <carmezim.filho@gmail.com>
2016-12-12 23:50:20 +0200

    moving play, pause, resume feat to ArticleItem

--- src/components/ArticleList/ArticleList.js
+++ src/components/ArticleList/ArticleList.js
@@ -1,44 +1,16 @@
 import React from 'react';
 import ArticleListItem from '../ArticleListItem/ArticleListItem';
-import speech from '../../services/Speech';
+// import speech from '../../services/Speech';
 
 class ArticleList extends React.Component {
   playing = this.props.playing;
-
-  handleClick(articleToPlay) {
-    switch(this.playing) {
-      case 'FIRST_PLAY':
-        console.log('firstplay');
-        speech.articleContent = articleToPlay;
-        speech.play();
-        this.playing = 'PLAYING';
-        break;
-
-      case 'PLAYING':
-        console.log('pause');
-        speech.pause();
-        this.playing = 'PAUSED';
-        break;
-
-      case 'PAUSED':
-        console.log('resume');
-        speech.resume();
-        this.playing = 'PLAYING';
-        break;
-
-      default: console.log('player error');
-        break;
-    }
-  }
-
   render() {
     let article;
     if(this.props.articles) {
       article = this.props.articles.map((articleItem, index) => {
         return(
           <div key={index}>
-            <button onClick={() => this.handleClick(articleItem.content)}>Btn</button>
-            <ArticleListItem headline={articleItem.headline} />
+            <ArticleListItem playing={this.playing} articleContent={articleItem.content} headline={articleItem.headline} />
           </div>
         );
       });
--- src/components/ArticleListItem/ArticleListItem.js
+++ src/components/ArticleListItem/ArticleListItem.js
@@ -1,10 +1,41 @@
-import React from 'react';;
+import React from 'react';
+import speech from '../../services/Speech';
+
 
 class ArticleListItem extends React.Component {
+  playing = this.props.playing;
+
+  handleClick(articleToPlay) {
+    switch(this.playing) {
+      case 'FIRST_PLAY':
+        console.log('firstplay');
+        speech.articleContent = articleToPlay;
+        speech.play();
+        this.playing = 'PLAYING';
+      break;
+
+      case 'PLAYING':
+        console.log('pause');
+        speech.pause();
+        this.playing = 'PAUSED';
+      break;
+
+      case 'PAUSED':
+        console.log('resume');
+        speech.resume();
+        this.playing = 'PLAYING';
+      break;
+
+      default: console.log('player error');
+      break;
+    }
+  }
+
   render() {
     return (
       <div className="article-list-item">
         <li>
+          <button onClick={() => this.handleClick(this.props.articleContent)}>Btn</button>
           <h3>{this.props.headline}</h3>
         </li>
       </div>
