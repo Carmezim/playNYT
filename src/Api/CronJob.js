1744d7f4a90f981b610d0ab06fa4f025abf49f63
Adriano Carmezim <Carmezim.filho@gmail.com>
2016-12-27 07:10:26 +0200

    Delete CronJob.js

--- src/Api/CronJob.js
+++
@@ -1,8 +0,0 @@
-import articlesLinks from './ArticleLinks';
-var CronJob = require('cron').CronJob;
-
-var job = new CronJob('* */1 * * * *', function () {
-  articlesLinks();
-},null, true, 'America/New_York');
-
-export default job;
