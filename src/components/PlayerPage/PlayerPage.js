2127e66e9c4862a4397b2d030d7ea393054135e2
Carmezim <carmezim.filho@gmail.com>
2017-01-07 02:45:39 +0000

    updating

--- src/components/PlayerPage/PlayerPage.js
+++ src/components/PlayerPage/PlayerPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ArticleList from '../ArticleList/ArticleList';
 import Player from '../Player/Player';
-import job from '../../services/CronJob';
+// import job from '../../services/CronJob';
 import articlesData from '../../../articles.json';
 
 class PlayerPage extends React.Component {
@@ -20,11 +20,9 @@ class PlayerPage extends React.Component {
       initialized: false
     }
   }
-  // Pass headline of article clicked to be displayed on Player component, validating it. Pass "playing" state
-  // Function is passed through props to ArticleList component and then to ArticleListItem component where it is executed
-  // to handle the clicks on articles.
+  // Handle state changes through props from children to parent component.
   clickHandlerPlayer = (isPlaying, isInitialized, headlineOnPlayer, articleContent) => {
-    let nextState = Object.assign({}, this.state, {playing: isPlaying, initialized: isInitialized, headlinePlay: headlineOnPlayer, content: articleContent});
+    let nextState = Object.assign({}, this.state, {playing: isPlaying, initialized: isInitialized, headlinePlay: headlineOnPlayer, content: articleContent      });
     this.setState(this.validateHeadline(nextState));
   }
   // validates if headline variable isn't empty
@@ -46,7 +44,7 @@ class PlayerPage extends React.Component {
   }
 
   componentDidMount(){
-    job();
+    // job();
   }
 
   render() {
