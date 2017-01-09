20ccf3e6f16e2640b5be00712fb1ef82daaf2172
Carmezim <carmezim.filho@gmail.com>
2017-01-09 21:04:09 +0000

    adding alt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>playNYT <img src="http://icons.iconarchive.com/icons/martz90/circle-addon1/512/new-york-times-icon.png"/></h1>
+        <h1>playNYT <img src="http://icons.iconarchive.com/icons/martz90/circle-addon1/512/new-york-times-icon.png" alt="NYT-logo"/></h1>
         <div className="moto top">Same Iconic Content</div>
         <div className="moto bottom">New Experience</div>
         <PlayerPage />
