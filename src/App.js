5bfeda933be9efb42912c0b00da07228b669b03c
Carmezim <carmezim.filho@gmail.com>
2017-01-09 01:12:52 +0000

    testing logo andand desktop lateral text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <h1>playNYTimes</h1>
+        <h1>playNYT <img src="http://icons.iconarchive.com/icons/martz90/circle-addon1/512/new-york-times-icon.png"/></h1>
+        <div className="moto top">Same Iconic Content</div>
+        <div className="moto bottom">New Experience</div>
         <PlayerPage />
         <footer> © 2017 Adriano Carmezim Filho. All articles material and logo belong to The New York Times Company. All Rights Reserved. Font made by <a href="http://www.onlinewebfonts.com">Online Web Fonts</a> is licensed by CC BY 3.0.
         </footer>
